016294b71f862195373454b4763a88a563391533
ubeshkrishnanNAME <ubeshkrishnan126@gmail.com>
2023-04-19 10:02:07 +0530

    update menu file

--- client/src/Global_variable/api_link.js
+++ client/src/Global_variable/api_link.js
@@ -1,5 +1,5 @@
-const Url= "http://52.66.24.38:5005";
-// const Url= "http://localhost:5005";
+// const Url= "http://52.66.24.38:5005";
+const Url= "http://localhost:5005";
 const   File_Url=  "http:localhost:5005/uploads/";
 const    homepage_url = "http://181.215.78.5:3004";
   

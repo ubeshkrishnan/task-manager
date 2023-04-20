8a62af13bb94a6e9562dc5ca92851710ac5971a3
ubeshkrishnan <103400634+ubeshkrishnan@users.noreply.github.com>
2023-04-20 22:37:29 +0530

    api commit

--- client/src/Global_variable/api_link.js
+++ client/src/Global_variable/api_link.js
@@ -1,4 +1,4 @@
-const Url = "http://65.2.167.215:5004/";
+const Url = "http://65.2.167.215:5004";
 // const Url= "http://localhost:5005";
 const File_Url = "http:localhost:5005/uploads/";
 const homepage_url = "http://181.215.78.5:3004";

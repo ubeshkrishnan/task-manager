7c64e7dade38976de604b92aa54937f652e3186d
ubeshkrishnanNAME <ubeshkrishnan126@gmail.com>
2023-04-21 12:29:24 +0530

    git api commit

--- client/src/Global_variable/api_link.js
+++ client/src/Global_variable/api_link.js
@@ -1,4 +1,4 @@
-const Url= "http://65.2.186.13:5004";
+const Url= "http://3.110.105.120:5004";
 // const Url= "http://localhost:5005";
 const   File_Url=  "http:localhost:5005/uploads/";
 const    homepage_url = "http://181.215.78.5:3004";

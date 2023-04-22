ac59b0446c53b1d7f9979afcfd6cc6b0ff85b4bd
ubeshkrishnanNAME <ubeshkrishnan126@gmail.com>
2023-04-22 11:40:36 +0530

    commit

--- client/src/Global_variable/api_link.js
+++ client/src/Global_variable/api_link.js
@@ -3,7 +3,9 @@ const Url= "http://65.2.167.215:5004";
 
 
 // const Url= "http://localhost:5005";
+
 const File_Url = "http:localhost:5005/uploads/";
+
 const homepage_url = "http://181.215.78.5:3004";
 
 export { Url, File_Url, homepage_url };
